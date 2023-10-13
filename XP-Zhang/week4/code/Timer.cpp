dbea1819532a44bbbd7e666b2b5e871f01ea463a
renlei505 <2393354272@qq.com>
2023-10-13 21:48:15 +0800

    ZXP

---
+++ XP-Zhang/week4/code/Timer.cpp
@@ -0,0 +1,14 @@
+class Timer
+{
+private:
+    typedef std::chrono::time_point<std::chrono::system_clock> time_t;
+    time_t startTime,stopTime;
+public:
+    Timer() : startTime(),stopTime(){}
+
+    time_t start() {return (startTime = std::chrono::system_clock::now());}
+    time_t stop() {return (stopTime = std::chrono::system_clock::now());}
+    double elapsed(){
+        return std::chrono::duration_cast<std::chrono::milliseconds>(stopTime-startTime).count();
+    }
+};
